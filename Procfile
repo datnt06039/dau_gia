a2b56144da9b043c91dfb14df075a8cb8be176b2
hapthse05058 <phamhongha.innerpiece@gmail.com>
2020-07-28 17:02:49 +0700

    fix package.json file to deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node app_module.js
--- app_module.js
+++ app_module.js
@@ -1,4 +1,4 @@
-var PORT = process.env.PORT || 3003;
+var PORT = process.env.PORT || 3003
 const mod = require('./module')
 const express = require('express')
 const app = express()
